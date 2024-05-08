6b69619d1a369e405c084867bd853eb91293178c
Carla Antonini <carla.antonini@hotmail.com>
2024-05-07 21:39:23 -0300

    CA configuracion por ambientes, modificacion de archivo settings por  nuevo archivo base.py y nuevos fiel development y production

---
+++ BACK-END/virtual-trends/config/settings/base.py
@@ -0,0 +1,126 @@
+from pathlib import Path
+
+# Cambio el base directory, porque ahora la carpeta root del proyecto es config porque es donde está el base.py con las settings que antes estaba en la raiz con el nombre del proyecto virtual-trends
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# El valor de la secret key depende del ambiente, si es para desarrollo suele ser sencillo tipo 1234 pero si es de produccion conviene que sea elaborada por temas de seguridad
+# TODO SECRET_KEY = 'django-insecure-a9=qz^k!f3!#+!aj_!15gu9^5sii%@@w%iru3z5gquma_4(9j-'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# TODO DEBUG = True
+
+#Depende del ambiente. En produccion no se puede dejar vacío, es dónde se va a encontrar la aplicacion virtual trends django
+# TODO ALLOWED_HOSTS = []
+
+
+# Divido las apps de installed_apps (ahora base) van las de django, segun sean apps de base, locales, o terceras aplicaciones que usamos
+
+BASE_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+LOCAL_APPS = []
+
+THIRD_APPS = []
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS +THIRD_APPS
+
+#Dividimos los middlewares en los de django (base), local los que vamos a crear, third los externos
+BASE_MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+LOCAL_MIDDLEWARE = []
+
+THIRD_MIDDLEWARE = []
+
+MIDDLEWARE = BASE_MIDDLEWARE + LOCAL_MIDDLEWARE + THIRD_MIDDLEWARE
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# COMENTO ESTO PORQUE DEPENDE DEL CONTEXTO DE EJECUCION / AMBIENTE Las bd son diferentes en local y produccion
+""" DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
