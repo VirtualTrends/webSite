259e10879af74a7991410a16547de1047fd9c85a
Carla Antonini <carla.antonini@hotmail.com>
2024-05-08 04:58:08 -0300

    CA modif  archivo ENTRYPOINT.sh ageego linea primera de bin/sh palabra para los comandos de produccion

--- BACK-END/virtual-trends/entrypoint.sh
+++ BACK-END/virtual-trends/entrypoint.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 echo 'Running collecstatic'
 python3 manage.py collectstatic --no-input --settings=config.settings.production
 echo 'Applying migrations...'
 python3 manage.py migrate --settings=config.settings.production
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application -- 127.0.0:8000
+gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 127.0.0:8000
