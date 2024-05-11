4afbb8b152d72e5b76168a70ae984dba997f4b18
Carla Antonini <carla.antonini@hotmail.com>
2024-05-11 09:02:15 -0300

    CA AGREGO USO DE LOGGING para poder ayudar en la lectura dee la ejecución del codigo

--- BACK-END/virtual-trends/config/logging.py
+++ BACK-END/virtual-trends/config/logging.py
@@ -1,15 +1,15 @@
 import os
 
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
             "class": "logging.StreamHandler",
         },
     },
-    "root": {
-        "handlers": ["console"],
-        "level": "WARNING",
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
     },
 }
