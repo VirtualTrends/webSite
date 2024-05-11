82c0f9c9a816ae8ac9da4023d6f3e53899444348
Carla Antonini <carla.antonini@hotmail.com>
2024-05-11 08:58:16 -0300

    CA agrego archivo final sh para el entrypoint de aplicacion de comandos dentro de docker

--- BACK-END/virtual-trends/entrypoint.sh
+++ BACK-END/virtual-trends/entrypoint.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
+
 echo 'Running collecstatic'
 python3 manage.py collectstatic --no-input --settings=config.settings.production
+
 echo 'Applying migrations...'
+python3 manage.py wait_for_db --settings=config.settings.production
 python3 manage.py migrate --settings=config.settings.production
+
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 127.0.0:8000
+
+#gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 127.0.0.1:8000
+
+#gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:8000
+
+#gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:$PORT
+
+gunicorn \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \       # Ajusta el número de trabajadores según tu necesidad
+    --timeout 120 \     # Ajusta el tiempo de espera según tu necesidad
+    --env DJANGO_SETTINGS_MODULE=config.settings.production \
+    config.wsgi:application
