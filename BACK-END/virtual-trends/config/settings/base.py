18bafa1da03565592afbd37244c5aef8b2edaa7f
Carla Antonini <carla.antonini@hotmail.com>
2024-05-11 08:56:50 -0300

    CA agrego carpeta para configuraciones segun entorno, desarrollando un archivo base.py que reemplaza el archivo de settings original

--- BACK-END/virtual-trends/config/settings/base.py
+++ BACK-END/virtual-trends/config/settings/base.py
@@ -7,16 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# El valor de la secret key depende del ambiente, si es para desarrollo suele ser sencillo tipo 1234 pero si es de produccion conviene que sea elaborada por temas de seguridad
-# SECRET_KEY = 'django-insecure-a9=qz^k!f3!#+!aj_!15gu9^5sii%@@w%iru3z5gquma_4(9j-'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-
-#Depende del ambiente. En produccion no se puede dejar vacío, es dónde se va a encontrar la aplicacion virtual trends django
-# ALLOWED_HOSTS = []
-
 
 # Divido las apps de installed_apps (ahora base) van las de django, segun sean apps de base, locales, o terceras aplicaciones que usamos
 
@@ -33,7 +23,9 @@ LOCAL_APPS = [
     'apps.core'
 ]
 
-THIRD_APPS = []
+THIRD_APPS = [
+
+]
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS +THIRD_APPS
 
@@ -48,7 +40,9 @@ BASE_MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-LOCAL_MIDDLEWARE = []
+LOCAL_MIDDLEWARE = [
+
+]
 
 THIRD_MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -76,16 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-# COMENTO ESTO PORQUE DEPENDE DEL CONTEXTO DE EJECUCION / AMBIENTE Las bd son diferentes en local y produccion
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
 
 
 # Password validation
